e947c8601bcb9c47a863f3e13e7647279d80d860
CarlosD333 <46306335+CarlosD333@users.noreply.github.com>
2019-01-31 21:56:39 -0600

    Update Carlos1a.java

--- Carlos1a.java
+++ Carlos1a.java
@@ -2,8 +2,6 @@ public class Carlos1a
 {
 	public static void main( String[] args )
 	{
-		System.out.println( "Carlos J. Degollado" );
-		System.out.println();
-		System.out.println( "C-Degollado@neiu.edu" );
+		
 	}
 }
