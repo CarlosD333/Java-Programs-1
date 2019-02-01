73aa1cc31c49e0a26593a1ddc58aed901da98668
CarlosD333 <46306335+CarlosD333@users.noreply.github.com>
2019-01-31 21:52:02 -0600

    Add files via upload

---
+++ Carlos1a.java
@@ -0,0 +1,9 @@
+public class Carlos1a
+{
+	public static void main( String[] args )
+	{
+		System.out.println( "Carlos J. Degollado" );
+		System.out.println();
+		System.out.println( "C-Degollado@neiu.edu" );
+	}
+}
