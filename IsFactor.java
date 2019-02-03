3589de6f6d6d334f56846bed95e8b147c50a5c87
CarlosD333 <46306335+CarlosD333@users.noreply.github.com>
2019-02-03 00:26:59 -0600

    Update IsFactor.java

--- IsFactor.java
+++ IsFactor.java
@@ -11,17 +11,32 @@ public class IsFactor
       System.out.print( "Enter the second number: " );
       int num2 = input.nextInt();
       
-       
-      
       System.out.println();
-      if(  )
+      
+      
+      if( num1 % num2 == 0 || num2 % num1 == 0 )
       {
-         System.out.print( small + " is a factor of " + big );
+         if( num1 < num2 )
+         {
+            System.out.print( num1 + " is a factor of " + num2 );
+         }
+         else
+         {
+            System.out.print( num2 + " is a factor of " + num1 );
+         }
       }
       else
       {
-         System.out.print( small + " is not a factor of " + big );  
+         if( num1 > num2 )
+         {
+            System.out.print( num2 + " is not a factor of " + num1 );
+         }
+         else
+         {
+            System.out.print( num1 + " is not a factor of " + num2 );
+         }
       }
-      
+       
+            
    }
 }
