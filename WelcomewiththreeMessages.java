26374600bf8b763d5d91ee03f4352ef91bc71c7a
CarlosD333 <46306335+CarlosD333@users.noreply.github.com>
2019-02-26 09:31:26 -0600

    Update WelcomewiththreeMessages.java

--- WelcomewiththreeMessages.java
+++ WelcomewiththreeMessages.java
@@ -1,4 +1,4 @@
-public class WelcomewiththreeMessages { 
+public class WelcomeWithThreeMessages { 
 public static void main(String[]args) { 
 System.out.println("Programming is fun!"); 
 System.out.println("Fundamentals First"); 
