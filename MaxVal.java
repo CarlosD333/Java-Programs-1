007cb957511bd4834b05f017e748504ee004551a
Carlos J Degollado <46306335+CarlosD333@users.noreply.github.com>
2020-02-07 19:54:19 -0600

    Add files via upload

---
+++ MaxVal.java
@@ -0,0 +1,41 @@
+package com.company;
+
+/*public class MaxVal {
+    public static int maxVal(int a, int b) {
+        int x = Math.max(a,b);
+        return x;
+    }
+    public static int maxVal(int a, int b, int c) {
+        int x = Math.max(a,b);
+        int y = Math.max(x,c);
+        return y;
+    }
+}
+*/
+
+/*
+public class MaxVal {
+    public static double maxVal(int a, int b) {
+        int x = Math.max(a,b);
+        return x;
+    }
+    public static double maxVal(double a, int b) {
+        double x = Math.max(a,b);
+        double y = Math.max(x,c);
+        return y;
+    }
+
+ */
+
+public class MaxVal {
+    public static double maxVal(int a, double b) {
+        double x = Math.max(a,b);
+        return x;
+    }
+//    public static double maxVal(double a, int b) {
+//        double x = Math.max(a,b);
+//       // double y = Math.max(x);
+//        return y;
+//    }
+
+}
