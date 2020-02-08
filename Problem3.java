3cf2fe4ec8bd7a90c6db7133deb6dd0f82f90380
Carlos J Degollado <46306335+CarlosD333@users.noreply.github.com>
2020-02-07 19:51:41 -0600

    Add files via upload

---
+++ Problem3.java
@@ -0,0 +1,36 @@
+package com.carlosdegollado;
+
+public class Problem3 {
+
+    private String freq;
+    private String text;
+
+    public Problem3(String freq, String text) {
+        this.freq = freq;
+        this.text = text;
+    }
+
+    public String repeatChars() {
+        String result = "";
+        String str = this.text;
+        String numString = this.freq;
+        boolean flag = true;
+
+
+        for (int i = 0; i < str.length(); i++) {
+            char letter = str.charAt(i);
+            char digit = numString.charAt(i);
+            int num = Character.getNumericValue(digit);
+
+            for (int j = 0; j < num; j++) {
+                result += letter;
+            }
+        }
+
+        return result;
+    }
+
+    public void displayRepeatChars() {
+        System.out.println(repeatChars());
+    }
+}
